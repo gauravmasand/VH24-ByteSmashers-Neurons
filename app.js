3e51353b1782efa368ca3df815783fcc11d06132
JayeshPatil18 <103875370+JayeshPatil18@users.noreply.github.com>
2024-10-04 14:09:34 +0530

    Update app.js
    fkjsdhkfhsdkjfh

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Node.js API!');
+    res.send('Welcome to the Node js API!');
 });
 
 // Routes
